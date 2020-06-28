32abe0e73e86372f144b54debc1e26bb74a843e6
akgupta89 <akgupta89@users.noreply.github.com>
2020-06-28 07:16:05 +0000

    deploy: akgupta89/lightscreen@cd6e42b755c218525d39e3539592adf587cb6d73

---
+++ lightscreen/precache-manifest.e755ba69ffca3f3da42ff4cc9f138ee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea472f972729ae4bf96a407d4dca75f",
+    "url": "/lightscreen/index.html"
+  },
+  {
+    "revision": "1cbd009dc8a9ca44c2bd",
+    "url": "/lightscreen/static/js/2.46119cf0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/lightscreen/static/js/2.46119cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f335dfa2e86e84bab862",
+    "url": "/lightscreen/static/js/main.45128854.chunk.js"
+  },
+  {
+    "revision": "aa8ad3f63a42f689a71a",
+    "url": "/lightscreen/static/js/runtime-main.6ce89fd9.js"
+  }
+]);
